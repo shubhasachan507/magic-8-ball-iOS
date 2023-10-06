221c40ee23eb63539db052b024486415eab2b196
Shubha-Sachan_tdpl <shubha.sachan@tatadigital.com>
2023-10-06 19:22:45 +0530

    Initial Commit

---
+++ magic-8-ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic-8-ball
+//
+//  Created by Shubha Sachan on 06/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
